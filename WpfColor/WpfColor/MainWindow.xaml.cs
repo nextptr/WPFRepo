1df4daa8d47eee983b23b75a37a0c0784c310af5
Zjw <762259059@qq.com>
2021-02-24 16:13:24 +0800

    add animation demo

---
+++ WpfColor/WpfColor/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfColor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    /// 
+
+    public class BrushItem
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Brush brush;
+        public Brush Brush
+        {
+            get { return brush; }
+            set { brush = value; }
+        }
+
+        public BrushItem(string nam,Brush bsh)
+        {
+            Name = nam;
+            Brush = bsh;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public List<BrushItem> ls = new List<BrushItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (this.IsVisible == true)
+            {
+                PropertyInfo[] pi = typeof(Brushes).GetProperties();
+                int count = pi.Length;
+                double tmp = Math.Sqrt(count);
+                if (tmp % 1 > 0)
+                {
+                    tmp++;
+                }
+                int sz = (int)tmp;
+
+                // 获取所有静态公共属性
+                foreach (PropertyInfo item in pi)
+                {
+                    ls.Add(new BrushItem(item.Name, (Brush)item.GetValue(null, null)));
+                }
+
+                InitCells(sz);
+
+                InitBinding(sz, ls);
+            }
+        }
+
+        public void InitCells(int count)
+        {
+            //动态生成布局器
+            for (int i = 0; i < count; i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                rd.Height = new GridLength(1, GridUnitType.Star);
+                gd.RowDefinitions.Add(rd);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                cd.Width = new GridLength(1, GridUnitType.Star);
+                gd.ColumnDefinitions.Add(cd);
+            }
+        }
+        public void InitBinding(int count, List<BrushItem> ls)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    int idx = i * count + j;
+                    if (idx >= ls.Count)
+                    {
+                        break;
+                    }
+
+                    Button btn = new Button();
+                    btn.ApplyTemplate();
+                    btn.VerticalAlignment = VerticalAlignment.Stretch;
+                    btn.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    btn.Content= ls[idx].Name;
+                    btn.Background= ls[idx].Brush;
+                    btn.Click += Btn_Click;
+
+                    gd.Children.Add(btn);
+                    Grid.SetColumn(btn, j);
+                    Grid.SetRow(btn, i);
+                }
+            }
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            //if (files == null) return;
+            //IDataObject data = new DataObject(DataFormats.FileDrop, files);
+            //MemoryStream memo = new MemoryStream(4);
+            //byte[] bytes = new byte[] { (byte)(cut ? 2 : 5), 0, 0, 0 };
+            //memo.Write(bytes, 0, bytes.Length);
+            //data.SetData("PreferredDropEffect", memo);
+            //Clipboard.SetDataObject(data, false);
+
+            //string[] file = new string[1];
+            //file[0] = "d://logo.png";
+            //DataObject dataObject = new DataObject();
+            //dataObject.SetData(DataFormats.FileDrop, file);
+            //Clipboard.SetDataObject(dataObject, true);
+
+
+            //Button btn = sender as Button;
+            //Clipboard.SetText(btn.Content.ToString());
+            //剪切板打开失败的相关博客，https://www.cnblogs.com/guogangj/p/7465951.html
+
+
+            Button btn = sender as Button;
+            Clipboard.SetDataObject(btn.Content.ToString());
+        }
+    }
+}
---
+++ WpfDemo/WpfAnimationDemo/TableMove/TransferTableItem.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAnimationDemo.TableMove
+{
+    /// <summary>
+    /// TransferTableItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class TransferTableItem : UserControl
+    {
+        public string Title { get; set; }
+
+        private Storyboard storyboard = null;
+        private FrameworkElement _element = null;
+        private DoubleAnimation anim_x = null;
+        private DoubleAnimation anim_y = null;
+
+        private Queue<animItem> animationList = new Queue<animItem>();
+
+        public TransferTableItem()
+        {
+            InitializeComponent();
+
+            //动画初始化
+            _element = this;
+            storyboard = new Storyboard();
+            anim_x = new DoubleAnimation();
+            anim_y = new DoubleAnimation();
+
+            storyboard = new Storyboard();
+            //创建X轴方向动画 
+            Storyboard.SetTarget(anim_x, _element);
+            Storyboard.SetTargetProperty(anim_x, new PropertyPath("(Canvas.Left)"));
+            //创建Y轴方向动画 
+            Storyboard.SetTarget(anim_y, _element);
+            Storyboard.SetTargetProperty(anim_y, new PropertyPath("(Canvas.Top)"));
+            storyboard.Completed += Storyboard_Completed;
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            toNextPos();
+        }
+
+        public void Start(Point pos_sta, Point pos_end,int time)
+        {
+            anim_x.From = pos_sta.X;
+            anim_x.To = pos_end.X;
+            anim_x.Duration = new Duration(TimeSpan.FromMilliseconds(time));
+
+            anim_y.From = pos_sta.Y;
+            anim_y.To = pos_end.Y;
+            anim_y.Duration = new Duration(TimeSpan.FromMilliseconds(time));
+
+            storyboard.Children.Add(anim_x);
+            storyboard.Children.Add(anim_y);
+            storyboard.Begin();
+
+            //storyboard = new Storyboard();
+            ////创建X轴方向动画 
+            //DoubleAnimation doubleAnimation = new DoubleAnimation(
+            //                                  pos_sta.X,
+            //                                  pos_end.X,
+            //                                  new Duration(TimeSpan.FromMilliseconds(lxspeed))
+            //                                 );
+            //Storyboard.SetTarget(doubleAnimation, _element);
+            //Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("(Canvas.Left)"));
+            //storyboard.Children.Add(doubleAnimation);
+            ////创建Y轴方向动画 
+            //doubleAnimation = new DoubleAnimation(
+            //                  pos_sta.Y,
+            //                  pos_end.Y,
+            //                  new Duration(TimeSpan.FromMilliseconds(lyspeed))
+            //                 );
+            //Storyboard.SetTarget(doubleAnimation, _element);
+            //Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("(Canvas.Top)"));
+            //storyboard.Children.Add(doubleAnimation);
+            //动画播放 
+            //storyboard.Begin();
+        }
+
+        private void toNextPos()
+        {
+            animItem item = animationList.Dequeue();
+            anim_x.From = Canvas.GetLeft(this);
+            anim_x.To = item.pos.X;
+            anim_x.Duration = new Duration(TimeSpan.FromMilliseconds(item.Tim));
+
+            anim_y.From = Canvas.GetTop(this);
+            anim_y.To = item.pos.Y;
+            anim_y.Duration = new Duration(TimeSpan.FromMilliseconds(item.Tim));
+
+            storyboard.Children[0] = anim_x;
+            storyboard.Children[1] = anim_y;
+            storyboard.Begin();
+        }
+        public void toPos(Point pos_end, int time,bool isWait)
+        {
+            animationList.Enqueue(new animItem(pos_end, time));
+            if (animationList.Count == 0||isWait==false)
+            {
+                toNextPos();
+            }
+        }
+
+
+        public void RePlase( Point pos1,Point pos2 )
+        {
+            anim_x.From = pos1.X;
+            anim_x.To = pos2.X;
+
+            anim_y.From = pos1.Y;
+            anim_y.To = pos2.Y;
+
+            storyboard.Children[0] = anim_x;
+            storyboard.Children[1] = anim_y;
+            storyboard.Begin();
+        }
+
+        public void SetPose(Point pos)
+        {
+            SetValue(Canvas.LeftProperty, pos.X);
+            SetValue(Canvas.TopProperty, pos.Y);
+        }
+
+
+        public bool Pause()
+        {
+            if (storyboard != null)
+            {
+                storyboard.Pause(_element);
+                return true;
+            }
+            return false;
+        }
+        public bool Resume()
+        {
+            if (storyboard != null)
+            {
+                storyboard.Resume(_element);
+                return true;
+            }
+            return false;
+        }
+        public bool Stop()
+        {
+            if (storyboard != null)
+            {
+                storyboard.Stop(_element);
+                return true;
+            }
+            return false;
+        }
+
+
+        public void Lock(int tim)
+        {
+            Thread.Sleep(tim);
+        }
+    }
+
+    public class animItem
+    {
+        public Point pos = new Point();
+        public int Tim = 0;
+
+        public animItem()
+        {
+        }
+
+        public animItem(Point ps,int tm)
+        {
+            pos.X = ps.X;
+            pos.Y = ps.Y;
+            Tim = tm;
+        }
+    }
+
+}
