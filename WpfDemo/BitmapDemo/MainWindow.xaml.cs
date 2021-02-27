516225affc9d596d5137b895a38d7d2cabd70006
Zjw <762259059@qq.com>
2021-02-27 09:28:32 +0800

    add BitmapDemo

---
+++ WpfDemo/BitmapDemo/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BitmapDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        enum ImageFormat
+        {
+            JPG,
+            BMP,
+            PNG,
+            GIF,
+            TIF
+        }
+        string path = "";
+
+        private Bitmap bot;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnStart.Click += BtnStart_Click;
+            btnSave.Click += BtnSave_Click;
+            btnGrph.Click += BtnGrph_Click;
+            path = Directory.GetCurrentDirectory() + "test.BMP";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            FileStream sWr = new FileStream(path, FileMode.Create);
+            GenerateImage((BitmapSource)imgPanel.Source, ImageFormat.BMP, sWr);
+            sWr.Close();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            WriteableBitmap wb = new WriteableBitmap((int)this.ActualWidth, (int)this.ActualHeight, 96, 96, PixelFormats.Bgr32, null);
+            byte blue;
+            byte green;
+            byte red;
+            byte alpha;
+            int strid;
+            Int32Rect Rect;
+            Random rd;
+            rd = new Random();
+            for (int x = 0; x < wb.PixelWidth; x++)
+            {
+                for (int y = 0; y < wb.PixelHeight; y++)
+                {
+                    blue = (byte)rd.Next(0, 256);
+                    green = (byte)rd.Next(0, 256);
+                    red = (byte)rd.Next(0, 256);
+                    alpha = (byte)rd.Next(0, 256);
+                    byte[] colorData = { blue, green, red, alpha };
+                    Rect = new Int32Rect(x, y, 1, 1);
+                    strid = wb.PixelWidth * wb.Format.BitsPerPixel / 8;
+                    wb.WritePixels(Rect, colorData, strid, 0);
+                }
+            }
+            for (int x = 100; x < 110; x++)
+            {
+                for (int y = 0; y < wb.PixelHeight; y++)
+                {
+                    //blue  = (byte)21;
+                    //green = (byte)215;
+                    //red   = (byte)128;
+                    //alpha = (byte)111;
+                    blue = 0;
+                    green = 0;
+                    red = 0;
+                    alpha = 0;
+
+                    byte[] colorData = { blue, green, red, alpha };
+                    Rect = new Int32Rect(x, y, 1, 1);
+                    strid = wb.PixelWidth * wb.Format.BitsPerPixel / 8;
+                    wb.WritePixels(Rect, colorData, strid, 0);
+                }
+            }
+            for (int x = 0; x < wb.PixelWidth; x++)
+            {
+                for (int y = 100; y < 110; y++)
+                {
+                    blue = (byte)21;
+                    green = (byte)215;
+                    red = (byte)128;
+                    alpha = (byte)111;
+                    byte[] colorData = { blue, green, red, alpha };
+                    Rect = new Int32Rect(x, y, 1, 1);
+                    strid = wb.PixelWidth * wb.Format.BitsPerPixel / 8;
+                    wb.WritePixels(Rect, colorData, strid, 0);
+                }
+            }
+            imgPanel.Source = wb;
+
+            FileStream sWr = new FileStream(path, FileMode.Create);
+            GenerateImage(wb, ImageFormat.BMP, sWr);
+            sWr.Close();
+        }
+
+        private void GenerateImage(BitmapSource bitmap, ImageFormat format, Stream destStream)
+        {
+            BitmapEncoder encoder = null;
+
+            switch (format)
+            {
+                case ImageFormat.BMP:
+                    encoder = new BmpBitmapEncoder();
+                    break;
+                case ImageFormat.GIF:
+                    encoder = new GifBitmapEncoder();
+                    break;
+                case ImageFormat.JPG:
+                    encoder = new JpegBitmapEncoder();
+                    break;
+                case ImageFormat.PNG:
+                    encoder = new PngBitmapEncoder();
+                    break;
+                case ImageFormat.TIF:
+                    encoder = new TiffBitmapEncoder();
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            encoder.Save(destStream);
+        }
+
+
+        //https://blog.csdn.net/jiuzaizuotian2014/article/details/81279423
+        //利用不安全代码拷贝
+        private void BtnGrph_Click(object sender, RoutedEventArgs e)
+        {
+            Bitmap bot = new Bitmap(400, 400);
+            Graphics gs = Graphics.FromImage(bot);
+            System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            int num1 = 50;
+            int num2 = 50;
+            int num3 = 50;
+            gs.DrawLine(pen, num1, num2 + num3, num1, num2 - num3);
+            gs.DrawLine(pen, num1 - num3, num2, num1 + num3, num2);
+            gs.DrawRectangle(pen, 12, 12, 6, 6);
+
+            int bw = bot.Width, bh = bot.Height;
+            WriteableBitmap writeableBitmap = new WriteableBitmap(400, 400, 96, 96, PixelFormats.Bgra32, null);
+            int rPixelBytes = 400 * 400 * 4;
+            BitmapData data = bot.LockBits(new System.Drawing.Rectangle(0, 0, bw, bh), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            writeableBitmap.Lock();
+            unsafe
+            {
+                Buffer.MemoryCopy(data.Scan0.ToPointer(), writeableBitmap.BackBuffer.ToPointer(), rPixelBytes, rPixelBytes);
+            }
+            writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, bw, bh));
+            writeableBitmap.Unlock();
+            bot.UnlockBits(data);
+
+            imgPanel.Source = writeableBitmap;
+            bot.Dispose();
+            pen.Dispose();
+        }
+    }
+}
--- WpfDemo/WpfDemo/App.xaml.cs
+++ WpfDemo/WpfDemo/App.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WpfDemo
 {
@@ -13,5 +13,119 @@ namespace WpfDemo
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 程序启动
+        /// </summary>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            FrameworkCompatibilityPreferences.KeepTextBoxDisplaySynchronizedWithTextProperty = false;
+            AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+
+            //进程名称被修改
+            Process processes = Process.GetCurrentProcess();
+            if (processes.ProcessName != assembly.Name)
+            {
+                MessageBox.Show("错误:进程名称和程序集名称不统一!");
+                System.Environment.Exit(0);
+            }
+
+            //进程重复启动
+            Process[] myProcesses = Process.GetProcessesByName(assembly.Name);//获取指定的进程名
+            if (myProcesses.Length > 1)
+            {
+                MessageBox.Show("错误:程序已经启动!");
+                System.Environment.Exit(0);
+            }
+
+            //程序运行中
+            bool createNew = false;
+            Semaphore singleInstanceWatcher = new Semaphore(1, 1, assembly.Name, out createNew);
+            if (!createNew)
+            {
+                MessageBox.Show("错误:程序运行中!");
+                System.Environment.Exit(0);
+            }
+
+            //parameter.Init();//parameter数据加载
+            bool checkUser = true;  //用户登陆和密码验证
+            if (checkUser)
+            {
+                if (File.Exists(@"C:\Runtime"))
+                {
+                    //_initHardware(); //初始化硬件
+                    //
+                }
+
+                //显示主界面
+                //VM_WindowMain vm = new VM_WindowMain();
+                //vm.CloseCallBack += (s, el) =>
+                //{
+                //    _uninit();
+                //    System.Environment.Exit(0);
+                //};
+                //Application.Current.MainWindow = vm.UIElement as Window;
+                //vm.Show();
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
+        }
+
+        /// <summary>
+        /// 程序加载完成
+        /// </summary>
+        protected override void OnLoadCompleted(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+            Current.DispatcherUnhandledException += App_OnDispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        /// <summary>
+        /// UI线程抛出全局异常事件处理
+        /// </summary>
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 非UI线程抛出全局异常事件处理
+        /// </summary>
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                var exception = e.ExceptionObject as Exception;
+                if (exception != null)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 程序退出
+        /// </summary>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            Process.GetProcessById(Process.GetCurrentProcess().Id).Kill();
+            //System.Environment.Exit(0);
+        }
     }
 }
