cbb54d2f8df3c54818f364658e563ff7d68ee702
Zjw <762259059@qq.com>
2021-03-02 11:08:19 +0800

    文件处理以及控件style

---
+++ 文件处理/WpfApp/WpfApp/FileAnalyseHelper.cs
@@ -0,0 +1,250 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class filePathObj
+    {
+        public string fileName = "";
+        public string fullName = "";
+        public string fileDir = "";
+    }
+
+    public class FileAnalyseHelper
+    {
+        public filePathObj SelectFileDialog()
+        {
+            string str = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog().Value)
+            {
+                str = openFileDialog.FileName;
+            }
+
+            filePathObj ret = new filePathObj();
+            ret.fullName = str;
+            string[] arr = str.Split('\\');
+            if (arr.Length != 0)
+            {
+                string end = arr[arr.Length - 1];
+                ret.fileDir = str.TrimEnd(('\\' + end).ToCharArray());
+                ret.fileName = arr[arr.Length - 1].TrimEnd(".log".ToCharArray());
+            }
+            return ret;
+        }
+
+        private List<List<string>> getMotionRawData(filePathObj obj)
+        {
+            FileStream fs = new FileStream(obj.fullName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<List<string>> qrLs = new List<List<string>>();
+            string beg = "";
+            string tmp = "";
+            while (!sr.EndOfStream)
+            {
+                beg = sr.ReadLine();
+                while (beg.Contains("开始上料"))
+                {
+                    List<string> ls = new List<string>();
+                    ls.Add(beg.Substring(26, 10));
+                    while (!sr.EndOfStream)
+                    {
+                        tmp = sr.ReadLine();
+
+                        if (!tmp.Contains("开始上料"))
+                        {
+                            if (tmp.Contains("点灯压接") ||
+                                tmp.Contains("二次点灯") ||
+                                tmp.Contains("结束点灯"))
+                            {
+                                ;
+                            }
+                            else
+                            {
+                                ls.Add(tmp.Substring(26, 10));
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    beg = tmp;
+                    tmp = "";
+                    qrLs.Add(ls);
+                }
+            }
+            sr.Close();
+            fs.Close();
+            return qrLs;
+        }
+        private List<string> getAlignRawData(filePathObj obj)
+        {
+            FileStream fs = new FileStream(obj.fullName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<string> qrLs = new List<string>();
+            string beg = "";
+            while (!sr.EndOfStream)
+            {
+                beg = sr.ReadLine();
+                if (beg.Contains("图像处理#"))
+                {
+                    string[] arr = beg.Split('#');
+                    qrLs.Add(arr[1]);
+                }
+            }
+            sr.Close();
+            fs.Close();
+            return qrLs;
+        }
+        private List<string> getMemRawData(filePathObj obj)
+        {
+            FileStream fs = new FileStream(obj.fullName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<string> qrLs = new List<string>();
+            string beg = "";
+            while (!sr.EndOfStream)
+            {
+                beg = sr.ReadLine();
+                if (beg.Contains("#内存#"))
+                {
+                    qrLs.Add(beg);
+                }
+            }
+            sr.Close();
+            fs.Close();
+            return qrLs;
+        }
+
+        private List<List<int>> converToint(List<List<string>> qrLs)
+        {
+            List<List<int>> tmpls = new List<List<int>>();
+            for (int i = 0; i < qrLs.Count; i++)
+            {
+                List<int> tmpLs = new List<int>();
+                foreach (string str in qrLs[i])
+                {
+                    int.TryParse(str, out int D);
+                    tmpLs.Add(D);
+                }
+                tmpls.Add(tmpLs);
+            }
+
+            int[] lengthDic = new int[5000];
+            foreach (var ls in tmpls)
+            {
+                lengthDic[ls.Count]++;
+            }
+
+            int maxSize = 0;
+            int tmpCount = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (tmpCount < lengthDic[i])
+                {
+                    tmpCount = lengthDic[i];
+                    maxSize = i;
+                }
+            }
+
+            List<List<int>> retLs = new List<List<int>>();
+            foreach (var ls in tmpls)
+            {
+                if (ls.Count == maxSize)
+                {
+                    retLs.Add(ls);
+                }
+            }
+            return retLs;
+        }
+
+        private List<List<int>> converToRalaData(List<List<int>> qrLs)
+        {
+            List<List<int>> retLs = new List<List<int>>();
+            for (int i = 0; i < qrLs.Count; i++)
+            {
+                List<int> tmpLs = new List<int>();
+                for (int j = 1; j < qrLs[i].Count; j++)
+                {
+                    tmpLs.Add(qrLs[i][j] - qrLs[i][j - 1]);
+                }
+                retLs.Add(tmpLs);
+            }
+            return retLs;
+        }
+
+        private void saveAsRawData(filePathObj obj,List<List<int>> dataLs, string fileName)
+        {
+            FileStream fsw = new FileStream(obj.fileDir + $"\\{obj.fileName}_{fileName}.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fsw);
+
+            int maxSize = dataLs[0].Count;
+            List<List<string>> outLs = new List<List<string>>();
+            for (int i = 0; i < maxSize; i++)
+            {
+                List<string> tmpLs = new List<string>();
+                foreach (var ls in dataLs)
+                {
+                    if (ls.Count > i)
+                    {
+                        tmpLs.Add(ls[i].ToString()+" ");
+                    }
+                    else
+                    {
+                        tmpLs.Add("  ");
+                    }
+                }
+                outLs.Add(tmpLs);
+            }
+
+            foreach (var ls in outLs)
+            {
+                string lin = "";
+                foreach (string st in ls)
+                {
+                    lin += st;
+                }
+                sw.WriteLine(lin);
+            }
+            sw.Close();
+            fsw.Close();
+        }
+        private void saveAsRawData(filePathObj obj, List<string> dataLs, string fileName)
+        {
+            FileStream fsw = new FileStream(obj.fileDir + $"\\{obj.fileName}_{fileName}.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fsw);
+            foreach (var d in dataLs)
+            {
+                sw.WriteLine(d);
+            }
+            sw.Close();
+            fsw.Close();
+        }
+
+
+        public void analyseMotionData(filePathObj obj)
+        {
+            List<List<string>> rawLs = getMotionRawData(obj);
+            List<List<int>> rawDatas = converToint(rawLs);
+            saveAsRawData(obj, rawDatas, "初始数据");
+
+            List<List<int>> relDatas = converToRalaData(rawDatas);
+            saveAsRawData(obj, relDatas, "相对用时");
+        }
+        public void analyseAlignData(filePathObj obj)
+        {
+            List<string> rawLs = getAlignRawData(obj);
+            saveAsRawData(obj, rawLs, "焦点数据");
+        }
+        public void analyseMemoryData(filePathObj obj)
+        {
+            List<string> rawLs = getMemRawData(obj);
+            saveAsRawData(obj, rawLs, "内存数据");
+        }
+    }
+}
---
+++ 文件处理/WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn_clear.Click += Btn_clear_Click;
+            btn_motion_analyse.Click += Btn_motion_analyse_Click;
+            btn_align_analyse.Click += Btn_align_analyse_Click;
+            btn_memory_analyse.Click += Btn_memory_analyse_Click;
+        }
+
+        private void Btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            ls_box.Items.Clear();
+        }
+        private void addMsg(string str)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                ls_box.Items.Insert(0, str);
+            }));
+        }
+
+        private void Btn_motion_analyse_Click(object sender, RoutedEventArgs e)
+        {
+            FileAnalyseHelper hp = new FileAnalyseHelper();
+            filePathObj path = hp.SelectFileDialog();
+            addMsg("开始分析:"+path.fileName);
+            hp.analyseMotionData(path);
+            addMsg("处理完成");
+        }
+        private void Btn_align_analyse_Click(object sender, RoutedEventArgs e)
+        {
+            FileAnalyseHelper hp = new FileAnalyseHelper();
+            filePathObj path = hp.SelectFileDialog();
+            addMsg("开始分析:" + path.fileName);
+            hp.analyseAlignData(path);
+            addMsg("处理完成");
+        }
+        private void Btn_memory_analyse_Click(object sender, RoutedEventArgs e)
+        {
+            FileAnalyseHelper hp = new FileAnalyseHelper();
+            filePathObj path = hp.SelectFileDialog();
+            addMsg("开始分析:" + path.fileName);
+            hp.analyseMemoryData(path);
+            addMsg("处理完成");
+        }
+    }
+}
